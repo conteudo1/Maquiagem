1c1885289ce7a21e57a5f51acaab87e87ba5ddbb
conteudo1 <canvatrabalhosshow@gmail.com>
2026-06-05 20:59:13 -0300

    Add files via upload

---
+++ Maquiagem/script.js
@@ -0,0 +1,3 @@
+function mensagem(){
+    alert("Dica: Sempre limpe a pele, passe um hidratante e um primer antes da maquiagem para maior duração, e melhor resultados, tendo assim, uma pele protegida e forte para arrasar durante todo o dia!");
+}
